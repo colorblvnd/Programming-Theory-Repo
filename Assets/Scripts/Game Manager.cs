c247a8b9d04721f6220848dd461f433c5f48cd42
colorblvnd <140036573+colorblvnd@users.noreply.github.com>
2023-07-24 22:48:41 -0400

    Icon Follows Current Animal
    - The icon now follows and hovers above the currently selected animal

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject pauseScreen;
+    [SerializeField] private GameObject currentAnimalIcon;
+    [SerializeField] private Camera mainCamera;
     private bool gamePaused;
+    private Animal currentAnimal;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,28 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             TogglePause();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SelectCurrent();
+        }
+    }
+
+    public void SelectCurrent()
+    {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var animal = hit.collider.GetComponentInParent<Animal>();
+            currentAnimal = animal;
+        }
+
+        if (currentAnimal != null)
+        {
+            currentAnimalIcon.SetActive(true);
+            currentAnimalIcon.gameObject.transform.position = currentAnimal.transform.position + new Vector3(0, 4, 0);
         }
     }
 
